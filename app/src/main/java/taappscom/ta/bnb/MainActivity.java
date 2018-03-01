905686e79ae697d20e1a5a7b9f4e2a54647b27dd
Jugaljay <kochumannil05>
2018-03-02 00:57:49 +0530

    First Commit Aaane

---
+++ app/src/main/java/taappscom/ta/bnb/MainActivity.java
@@ -0,0 +1,16 @@
+package taappscom.ta.bnb;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Jugal on 25-02-2018.
+ */
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layout_2);
+    }
+}
